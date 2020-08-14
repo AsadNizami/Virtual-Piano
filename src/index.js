071ae713c5d85b0fcb7807146b87e6a2e55e3217
Asad <asadnizami12@gmail.com>
2020-08-14 05:58:49 +0530

    'first'

---
+++ src/index.js
@@ -0,0 +1,50 @@
+document.addEventListener('keydown', function(e){
+    if (e.key == 'a'){
+        let audio = new Audio("A.mp3");
+        audio.play()
+    }
+    if (e.key == 's'){
+        let audio = new Audio("S.mp3");
+        audio.play()
+    }
+    if (e.key == 'd'){
+        let audio = new Audio("D.mp3");
+        audio.play()
+    }
+    if (e.key == 'f'){
+        let audio = new Audio("F.mp3");
+        audio.play()
+    }
+    if (e.key == 'g'){
+        let audio = new Audio("G.mp3");
+        audio.play()
+    }
+    if (e.key == 'h'){
+        let audio = new Audio("H.mp3");
+        audio.play()
+    }
+    if (e.key == 'j'){
+        let audio = new Audio("J.mp3");
+        audio.play()
+    }
+    if (e.key == 'w'){
+        let audio = new Audio("W.mp3");
+        audio.play()
+    }
+    if (e.key == 'e'){
+        let audio = new Audio("E.mp3");
+        audio.play()
+    }
+    if (e.key == 't'){
+        let audio = new Audio("T.mp3");
+        audio.play()
+    }
+    if (e.key == 'y'){
+        let audio = new Audio("Y.mp3");
+        audio.play()
+    }
+    if (e.key == 'u'){
+        let audio = new Audio("U.mp3");
+        audio.play()
+    }
+})
